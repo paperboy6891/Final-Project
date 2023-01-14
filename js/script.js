cadf85c82b0654660cab98c9fb07e1ad8e482bbc
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-14 11:59:21 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-(function () {
-
+const final=()=> {
+    return(
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
-    for (var i = 0; i < names.length; i++) {
+    names.map((name,ind)=>(
+    let firstLetter=name.chartAt(0).toLowerCase();
+    ))
+//     for (var i = 0; i < names.length; i++) {
     
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } 
-      else {
-        helloSpeaker.speak(names[i]);
-      }
-    }
+//       var firstLetter = names[i].charAt(0).toLowerCase();
+//       if (firstLetter === 'j') {
+//         byeSpeaker.speak(names[i]);
+//       } 
+//       else {
+//         helloSpeaker.speak(names[i]);
+//       }
+//     }
     
-    })();
+    )}
+final();
